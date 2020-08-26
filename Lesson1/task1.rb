282bb911362efdf68bca88338042c7573a71fa11
Deniss Ponomarenko <ponomarenkoo.deniss@gmail.com>
2020-08-26 08:26:08 +0300

    Change style for Ruby

--- Lesson1/task1.rb
+++ Lesson1/task1.rb
@@ -2,4 +2,10 @@ puts "Как тебя зовут?"
 name = gets.chomp.to_s
 puts "#{name}, какого ты роста?"
 growth = gets.chomp.to_i
-puts (growth - 110) *1.15 < 0 ? "#{name}, Ваш вес уже оптимальный" : "#{name}, твой примерный вес - " + ((growth - 110) *1.15).to_i.to_s
+
+if ((growth - 110) * 1.15 < 0)
+  puts "#{name}, Ваш вес уже оптимальный"
+else
+  res = ((growth - 110) *1.15).to_i.to_s
+  puts "#{name}, твой примерный вес - " + res
+end
