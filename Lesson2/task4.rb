0b3a41c395daca44fd2f13b6828ebb7e3528e969
Deniss Ponomarenko <ponomarenkoo.deniss@gmail.com>
2020-08-26 21:35:14 +0300

    task 4 update

--- Lesson2/task4.rb
+++ Lesson2/task4.rb
@@ -1,17 +1,18 @@
 alphabet = ("a".."z").to_a #letters array
 needed   =  'a,e,i,o,u'.split(",") # array of vowels
 index    = 0
-numbers  = [] 
+#numbers  = [] 
 
 letters  = Hash.new 
 i = 0
 #get letter number
 while i < alphabet.size
   if needed.include?(alphabet[i])
-  	numbers.push(i)
+  	#numbers.push(i)
+  	letters[alphabet[i]] = i
   end
   i += 1
 end
 
-puts numbers
+puts letters
 
